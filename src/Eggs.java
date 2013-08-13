5d78580f1fd22c12abcb0f9ef1f56cab74650f0b
clydewbrown <clydewbrown@gmail.com>
2013-08-13 07:58:59 -0400

    Source files for Assignment #4

---
+++ src/Eggs.java
@@ -0,0 +1,17 @@
+
+public class Eggs {
+
+	public static void main(String[] args) {
+		// -------INSTRUCTIONS-------
+		// 1. Complete exercise 8 on page 114 of the Farrell book (chapter 2)
+		// 2. Then delete these comments
+		// 3. Finally, commit and push the project containing this file to GitHub
+		
+		// -------RUBRIC-------
+		// Functionality						/9
+		// Appropriate data types chosen		/5
+		// Coding conventions & comments		/6
+
+	}
+
+}
